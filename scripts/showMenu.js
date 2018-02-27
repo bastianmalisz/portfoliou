276b81c06d7fe3106d35d7749bef8d144a52dbea
bastianmalisz <malisz654321@gmail.com>
2018-02-28 00:48:11 +0100

    Podwaliny pod nowe protfolio

---
+++ scripts/showMenu.js
@@ -0,0 +1,27 @@
+const navBox = document.querySelector(".nav");
+const navButton = document.querySelector(".hamburger-icon");
+let buttonFlag = 0;
+let open = false;
+
+function showCloseNav(){
+    if(buttonFlag == 0 || buttonFlag%2 == 0 ){
+    navBox.style.right = 0;
+    navButton.style.right = 400+"px";
+    buttonFlag++;
+    open = true;
+    
+    }else{
+        navBox.style.right = -400+"px";
+        navButton.style.right = 0+"px";
+        buttonFlag++,open = false;
+        open = false;
+        
+        
+    }
+    this.classList.toggle('close');
+    
+   
+    }
+
+
+navButton.addEventListener("click",showCloseNav);
